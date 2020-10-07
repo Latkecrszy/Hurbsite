b4d88629029fa93c89776485e16b62f43e08dc7e
Seth Raphael <setharaphael7@gmail.com>
2020-10-07 10:17:24 -0700

    created flask app & created heroku config

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import make_response, jsonify, Flask
+
+
+app = Flask(__name__)
+
+@app.route('/todo')
+
+def todo():
+	data = [{"key1": "value1"}, {"key2": "value2"}]
+	res = make_response(jsonify(data))
+	res.mimetype = "application/json"
+	return res
+
+
+if __name__ == '__main__':
+	app.run()
+
+	
