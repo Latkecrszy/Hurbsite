0d2236070566d02690ac2faa06fb31284d55f106
Seth Raphael <setharaphael7@gmail.com>
2020-10-28 10:30:41 -0700

    changed key1: value1 to key: value

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import make_response, jsonify, Flask
-
+import os
 
 app = Flask(__name__)
 
-@app.route('/todo')
 
+@app.route('/todo')
 def todo():
-	data = [{"key1": "value1"}, {"key2": "value2"}]
+	data = [{"key": "value"}, {"key2": "value2"}]
 	res = make_response(jsonify(data))
 	res.mimetype = "application/json"
 	return res
 
 
 if __name__ == '__main__':
-	app.run()
-
-	
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
