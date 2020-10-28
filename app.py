0daaa4c0e06a025e6bd0aa6f806281aa3ac67b41
Seth Raphael <setharaphael7@gmail.com>
2020-10-28 10:46:49 -0700

    Added some HTML

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import make_response, jsonify, Flask
+from flask import make_response, jsonify, Flask, render_template
 import os
 
 app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+	return render_template('index.html')
 
 
 @app.route('/todo')
@@ -15,3 +20,5 @@ def todo():
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
+
+# https://still-castle-12476.herokuapp.com/todo
