c561ce538d10a5425dee5eb3f979d7ffaca8cdbc
Seth Raphael <setharaphael7@gmail.com>
2020-10-29 16:03:24 -0700

    attempted to edit html file

--- app.py
+++ app.py
@@ -47,10 +47,15 @@ class Leaderboard(commands.Cog):
 			newMemberOrder[min(memberOrder.keys())] = memberOrder[min(memberOrder.keys())]
 			memberOrder.pop(min(memberOrder.keys()))
 		num = 1
-
-	@commands.command()
-	async def leaderboard(self, ctx):
-		await ctx.send(embed=discord.Embed(title=f"Leaderboard for {ctx.guild}", description=f"[Click here to see the leaderboard](http://0.0.0.0:5000/{ctx.guild.id})"))
+		with open("/Users/sethraphael/PycharmProject/REST/templates/index.html", "r") as f:
+			file = f.read()
+		file = str(file)
+		file = file.split("f")
+		file[0] += "<div>Test</div>"
+		file = file[0] + file[1]
+		with open("/Users/sethraphael/PycharmProject/REST/templates/index.html", "w") as f:
+			f.write(file)
+		return render_template('index.html')
 
 	def position(self, member):
 		with open("/Users/sethraphael/PycharmProject/Hurb/Bots/rank.json") as f:
@@ -97,4 +102,3 @@ def setup(bot):
 
 
 # https://hurbsite.herokuapp.com/todo
-# https://discord.com/oauth2/authorize?access_type=online&client_id=204255083083333633&redirect_uri=https%3A%2F%2Fyagpdb.xyz%2Fconfirm_login&response_type=code&scope=identify+guilds&state=n0kCQj0D80GANM22xDLQBgTFpljgF311CruMY7uksEY%3D&prompt=none
