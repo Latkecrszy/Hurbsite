693ff625633ef727c15b7c38ff3d9529cecd47a8
Seth Raphael <setharaphael7@gmail.com>
2020-11-02 17:17:55 -0800

    renamed leaderboard template, made the site look good, files still probably don't work.

--- app.py
+++ app.py
@@ -7,22 +7,44 @@ import os
 import dotenv
 import requests
 import json
+import random
 
 app = Flask(__name__)
 
 app.config['TOKEN'] = os.environ.get('TOKEN', None)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
-
-
-@app.route('/<id>')
-def todo(id):
+@app.route('/leaderboard/<id>')
+def leaderboard(id):
     data = _leaderboard(id)
-    refreshHTML()
     return data
+
+
+@app.route('/profile/<id>')
+def profile(id):
+    pass
+
+
+@app.route('/joke', methods=['GET'])
+def joke():
+    jokes = ["Why did the chicken cross the road?<br>To get to the other side!",
+             "Why can't you write with a dull pencil?<br>Well, you can, but there's just no point."]
+    return f'''
+<html>
+    <body>
+        <p>{random.choice(jokes)}</p>
+    </body>
+</html>'''
+
+
+@app.route('/<Joke>', methods=['POST'])
+def Joke(Joke):
+    return f'''
+<html>
+    <body>
+        <p>{Joke}</p>
+    </body>
+</html>'''
 
 
 def _leaderboard(ID):
@@ -30,53 +52,39 @@ def _leaderboard(ID):
                      headers={"Authorization": f"Bot {app.config['TOKEN']}"})
     result = requests.get(f'https://discord.com/api/v7/guilds/{ID}/members?limit=1000',
                           headers={"Authorization": f"Bot {app.config['TOKEN']}"})
+    print(result.text)
+    print(r.text)
     result = result.json()
     r = r.json()
     memberOrder = {}
     newMemberOrder = {}
-    with open("/Users/sethraphael/PycharmProject/REST/hurbsite/scratch_1.json") as f:
+    with open("/Users/sethraphael/PycharmProject/Hurb/Bots/rank.json") as f:
         messages = json.load(f)
     members = messages[str(ID)]
     for id, stuff in members.items():
         for member in result:
             if member["user"]["id"] == str(id):
                 if "bot" not in member["user"].keys():
-                    memberOrder[position(r["id"], member["user"]["id"])] = [member["user"]["username"],
-                                                                            stuff["messages"], stuff["xp"],
-                                                                            stuff["level"]]
+                    if not str(member["user"]["avatar"]).startswith("a_"):
+                        memberOrder[position(r["id"], member["user"]["id"])] = [member["user"]["username"],
+                                                                                stuff["messages"], stuff["xp"],
+                                                                                stuff["level"],
+                                                                                f"https://cdn.discordapp.com/avatars/{member['user']['id']}/{member['user']['avatar']}.webp?size=1024", member["user"]["discriminator"]]
+                    else:
+                        memberOrder[position(r["id"], member["user"]["id"])] = [member["user"]["username"],
+                                                                                stuff["messages"], stuff["xp"],
+                                                                                stuff["level"],
+                                                                                f"https://cdn.discordapp.com/avatars/{member['user']['id']}/{member['user']['avatar']}.gif?size=1024", member["user"]["discriminator"]]
+
     for x in range(len(memberOrder.keys())):
         newMemberOrder[min(memberOrder.keys())] = memberOrder[min(memberOrder.keys())]
         memberOrder.pop(min(memberOrder.keys()))
-    num = 1
-    for key, value in newMemberOrder.items():
-        with open("/Users/sethraphael/PycharmProject/REST/templates/index.html", "r") as f:
-            file = f.read()
-        file = str(file)
-        file = file.split("f")
-        file[
-            0] += f"<div><b>#{num}: {value[0]}</b><br><i>Level: </i>{value[3]}<br><i>Messages: </i>{value[1]}<br><i>XP: </i>{value[2]}<br>&nbsp;<br></div>f"
-        file = file[0] + file[1]
-        with open("/Users/sethraphael/PycharmProject/REST/templates/index.html", "w") as f:
-            f.write(file)
-        num += 1
-    return render_template('index.html')
-
-
-def refreshHTML():
-    with open("/Users/sethraphael/PycharmProject/REST/templates/index.html", "w") as f:
-        f.write('''<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>Loading...</title>
-	</head>
-	<body>
-		f
-	</body>
-</html>''')
+    values = [value for value in newMemberOrder.values()]
+    return render_template('leaderboard.html', values=values, name=r["name"])
 
 
 def position(guild_id, member_id):
-    with open("/Users/sethraphael/PycharmProject/REST/hurbsite/scratch_1.json") as f:
+    with open("/Users/sethraphael/PycharmProject/Hurb/Bots/rank.json") as f:
         messages = json.load(f)
     messages = messages[str(guild_id)]
     XPs = {int(Member["xp"] + Member["level"] * (Member["level"] * 200)): ID for ID, Member in messages.items()}
