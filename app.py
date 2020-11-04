7bb2da47cd5b8082dcff300d0b6f4bb86de27856
Seth Raphael <setharaphael7@gmail.com>
2020-11-04 10:24:32 -0800

    added a post for the joke API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import make_response, jsonify, Flask, render_template
+from flask import make_response, jsonify, Flask, render_template, request
 import os
 import discord
 from discord.ext import commands
@@ -25,19 +25,28 @@ def profile(id):
     pass
 
 
+@app.route('/<joke>', methods=['POST'])
 @app.route('/joke', methods=['GET'])
-def joke():
-    jokes = ["Why did the chicken cross the road?<br>To get to the other side!",
-             "Why can't you write with a dull pencil?<br>Well, you can, but there's just no point."]
-    return f'''
-<html>
-    <body>
-        <p>{random.choice(jokes)}</p>
-    </body>
-</html>'''
+def joke(joke):
+    if request.method == "GET":
+
+        jokes = ["Why did the chicken cross the road?<br>To get to the other side!",
+                 "Why can't you write with a dull pencil?<br>Well, you can, but there's just no point."]
+        return f'''
+    <html>
+        <body>
+            <p>{random.choice(jokes)}</p>
+        </body>
+    </html>'''
+    elif request.method == "POST":
+        return f'''
+        <html>
+            <body>
+                <p>{joke}</p>
+            </body>
+        </html>'''
 
 
-@app.route('/<Joke>', methods=['POST'])
 def Joke(Joke):
     return f'''
 <html>
@@ -52,8 +61,6 @@ def _leaderboard(ID):
                      headers={"Authorization": f"Bot {app.config['TOKEN']}"})
     result = requests.get(f'https://discord.com/api/v7/guilds/{ID}/members?limit=1000',
                           headers={"Authorization": f"Bot {app.config['TOKEN']}"})
-    print(result.text)
-    print(r.text)
     result = result.json()
     r = r.json()
     memberOrder = {}
@@ -66,21 +73,43 @@ def _leaderboard(ID):
             if member["user"]["id"] == str(id):
                 if "bot" not in member["user"].keys():
                     if not str(member["user"]["avatar"]).startswith("a_"):
-                        memberOrder[position(r["id"], member["user"]["id"])] = [member["user"]["username"],
-                                                                                stuff["messages"], stuff["xp"],
-                                                                                stuff["level"],
-                                                                                f"https://cdn.discordapp.com/avatars/{member['user']['id']}/{member['user']['avatar']}.webp?size=1024", member["user"]["discriminator"]]
+                        memberOrder[position(r["id"], member["user"]["id"])] = [
+                            (member["nick"] if member["nick"] is not None else member["user"]["username"]),
+                            stuff["messages"], stuff["xp"],
+                            stuff["level"],
+                            f"https://cdn.discordapp.com/avatars/{member['user']['id']}/{member['user']['avatar']}.webp?size=1024",
+                            member["user"]["discriminator"],
+                            stuff["level"] * 200 + (200 * (
+                                int(stuff["level"] / 5) if int(
+                                    stuff[
+                                        "level"] / 5) >= 1 else 0)),
+                            calcspot(member["user"]["id"], r["id"])]
                     else:
-                        memberOrder[position(r["id"], member["user"]["id"])] = [member["user"]["username"],
-                                                                                stuff["messages"], stuff["xp"],
-                                                                                stuff["level"],
-                                                                                f"https://cdn.discordapp.com/avatars/{member['user']['id']}/{member['user']['avatar']}.gif?size=1024", member["user"]["discriminator"]]
+                        memberOrder[position(r["id"], member["user"]["id"])] = [
+                            (member["nick"] if member["nick"] is not None else member["user"]["username"]),
+                            stuff["messages"], stuff["xp"],
+                            stuff["level"],
+                            f"https://cdn.discordapp.com/avatars/{member['user']['id']}/{member['user']['avatar']}.gif?size=1024",
+                            member["user"]["discriminator"],
+                            stuff["level"] * 200 + (200 * (
+                                int(stuff["level"] / 5) if int(
+                                    stuff[
+                                        "level"] / 5) >= 1 else 0)),
+                            calcspot(member["user"]["id"], r["id"])]
 
     for x in range(len(memberOrder.keys())):
         newMemberOrder[min(memberOrder.keys())] = memberOrder[min(memberOrder.keys())]
         memberOrder.pop(min(memberOrder.keys()))
     values = [value for value in newMemberOrder.values()]
+    print(8 & 2)
     return render_template('leaderboard.html', values=values, name=r["name"])
+
+
+def adminRoles(member, guild):
+    for role in member["roles"]:
+        for i in guild["roles"]:
+            if str(i["id"]) == str(role):
+                pass
 
 
 def position(guild_id, member_id):
@@ -105,28 +134,17 @@ def position(guild_id, member_id):
     return numCount
 
 
+def calcspot(id, guild_id):
+    with open("/Users/sethraphael/PycharmProject/Hurb/Bots/rank.json") as f:
+        messages = json.load(f)
+    messages = messages[str(guild_id)]
+    level = messages[str(id)]["level"]
+    maxXP = level * 200 + (200 * (int(level / 5) if int(level / 5) >= 1 else 0))
+    return [int(messages[str(id)]["xp"]), int(maxXP)]
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
 
 # https://hurbsite.herokuapp.com/todo
-
-
-""".env
-#############################
-TOKEN=xxxxxxxxx
-
-install any Python dependencies
-pip install python-dotenv
-
-update requirements.txt
-
-confirm that .gitignore has .env as one of its lines
-
-# when you want to refer to your environ
-import os
-app.config['TOKEN'] = os.environ.get('TOKEN', None)
-app.config['TOKEN']
-
-heroku config:set TOKEN=xxxxxxxxx
-or use the user interface"""
